f5dd9d4232a2ff9443651c1f9ad28b1fff63a809
ABDIRASHID NUUR <78458279+abdinuur10@users.noreply.github.com>
2021-04-30 07:32:51 -0700

    Done

---
+++ App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Form from "./component/Form";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Form</h1>
+      <h2>Student Enrollment</h2>
+     <Form />
+    </div>
+  );
+}
